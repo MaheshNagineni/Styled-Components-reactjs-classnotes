afb69ab592463ea38a3c3368c21fe44a42a43190
mahesh <maheshb061@gmail.com>
2023-05-29 17:31:03 +0000

    submitted

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Heading, CustomButton} from './styledComponents'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Heading>Hello World</Heading>
+    <CustomButton type="button">Click</CustomButton>
+    <CustomButton type="button" outline>
+      Click
+    </CustomButton>
+    <CustomButton type="submit" onClick={() => alert('clicked')} outline>
+      Click
+    </CustomButton>
+    <CustomButton as="a" href="#" outline={true}>
+      Click
+    </CustomButton>
+  </>
+)
+
+export default App
